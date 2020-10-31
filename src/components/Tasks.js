0b67eca8d4adaed8e7d9fbc8c6b28c297f974bd8
Ekaji <ekajionah@gmail.com>
2020-10-31 21:40:41 +0100

    made minor changes

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {useEffect, useState} from 'react'
 import '../styles/Styles.css'
 
 const Tasks = ( {todos, deleat, status} ) => {
-
+   
     return(
         <div className = 'tasks-container '>
             {todos.map(todo => 
-                (<div className = 'content list'  key = {todo.id}> 
+                (<div className = {`content list ${todo.priority}`}  key = {todo.id}> 
                     <p className = {`task-item ${todo.completed? "strikeThrough" : "none"}`}> {todo.task} </p>
                     <span className = 'buttonArea'>
                         <button onClick = {() => status(todo)} > Done </button> 
