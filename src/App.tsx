b29c3cafd152d76169e4b243c79cab4af70a1a11
Ekaji <ekajionah@gmail.com>
2021-12-24 10:33:03 +0100

    rearanged repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useLocalStorage from '../src/hooks/useLocalStorage'
+import './App.css'
+import Input from './components/input'
+import Tasks from './components/tasks'
+import './styles/Styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App  = () => {
+
+  interface taskInterface {
+    id: Number;
+    completed: boolean;
+    task: string;
+    priority: string
+  }
+
+ const [LocalStorage, setLocalStorage] = useLocalStorage< taskInterface >('todos', [] )
+     
+    
+  type todoInterface = {
+    id: Number;
+    completed: boolean
+  }
+
+  const addTask = (todo: taskInterface) => {
+    if(!LocalStorage) return
+    try {
+      todo.id = LocalStorage.length + 1;
+      todo.completed = false; 
+      setLocalStorage( [...LocalStorage, todo] )
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const deleat = (id: Number) => {
+    const item = LocalStorage
+    setLocalStorage( item.filter((todo: todoInterface ) => todo.id !== id )
+  )
+}
+
+  const status = (todo: todoInterface) => {
+    todo.completed = !todo.completed;
+    console.log(todo.completed)
+    setLocalStorage([...LocalStorage])
+  }
+  
+  return(
+    <div className="centered">
+      <h1> Day to Day </h1>
+      <Input  addTask = { addTask } />
+      <Tasks  deleat = { deleat } status = { status } todos = { LocalStorage } />
     </div>
-  );
+  )
 }
 
 export default App;
